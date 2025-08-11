38922e7988bcdfbd44acfb18084de4b71eb51503
venice <92649256+Huda-bawazir@users.noreply.github.com>
2025-08-11 10:25:39 +0400

    11th day - plates counter
    spawns a plate every four seconds. A total of four plates. the player can pick up one plate at a time.

--- Assets/Scripts/Counters/PlatesCounter.cs
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -1,16 +1,46 @@
+using System;
 using UnityEngine;
 
 public class PlatesCounter : BaseCounter
 {
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
     private float spawnPlateTimer;
     private float spawnPlaterTimerMax = 4f;
-    [SerializeField] private KitchenObjectSO plateKitchenObjectSO; 
+    private int platesSpwanedAmount;
+    private int platesSpwanedAmountMax = 4;
     private void Update()
     {
-        spawnPlateTimer += Time.time;
+        spawnPlateTimer += Time.deltaTime;
         if (spawnPlateTimer > spawnPlaterTimerMax)
         {
-            KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, this); 
+            spawnPlateTimer = 0f;
+
+            //spwan a demo ( a number amount of plates ) 
+            if (platesSpwanedAmount < platesSpwanedAmountMax)
+            {
+                platesSpwanedAmount++;
+                OnPlateSpawned?.Invoke(this, new EventArgs());
+            }
+        }
+
+    }
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {   //player is empty handed, so we can give them a plate 
+            if(platesSpwanedAmount > 0)
+            { // Then there is alteast one plate here, let's give it to the player 
+                platesSpwanedAmount--; 
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+                OnPlateRemoved?.Invoke(this, new EventArgs());
+            }
+            
         }
     }
+
 }
