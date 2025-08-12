f6610491df8c7f12b329fc8269d08b6d65bb0ca1
venice <92649256+Huda-bawazir@users.noreply.github.com>
2025-08-12 13:56:18 +0400

    12th day - plate world UI icons
    Icons to display what's one the plate.

--- Assets/Scripts/PlateIconUI.cs
+++ Assets/Scripts/PlateIconUI.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class PlateIconUI : MonoBehaviour
 {
     [SerializeField] private PlateKitchenObject plateKitchenObject;
-    [SerializeField] private Transform iconTemplate; 
+    [SerializeField] private Transform iconTemplate;
 
+    private void Awake()
+    {
+       iconTemplate.gameObject.SetActive(false);
+    }
     private void Start()
     {
         plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
@@ -17,9 +21,19 @@ public class PlateIconUI : MonoBehaviour
 
     private void UpdateVisuals()
     {
-        foreach(KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+        //Clean up the icons so that it doesnt end up spawning too many icons.
+        foreach (Transform child in transform)
         {
-           Transform iconTransform = Instantiate(iconTemplate, transform); 
+            //do not destroy the icontemplate itself.
+            if (child == iconTemplate) continue; 
+            //this willl destroy all the previous children.
+            Destroy(child.gameObject); 
+        }
+        foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+        {
+           Transform iconTransform = Instantiate(iconTemplate, transform);
+            iconTransform.gameObject.SetActive(true);   
+           iconTransform.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectSO); 
         }
     }
 }
