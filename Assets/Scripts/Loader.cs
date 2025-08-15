310bd01a1093a17730b391ad9539d658c46e58ea
venice <92649256+Huda-bawazir@users.noreply.github.com>
2025-08-15 16:42:09 +0400

    15th day - Main Menu, Loading
    created a loader screen and a loadercallback object to be able to load the loading menue.

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    // must have static field in static class 
+    public static Scene targetScene; 
+
+    public enum Scene
+    {
+        MainMenu, 
+        GameScene, 
+        LoadingScene
+    }
+    public static void Load(Scene targetScene)
+    {
+        Loader.targetScene = targetScene;
+
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+
+        
+    }
+
+
+    //this function is going to be triggered at the first update.
+    public static void LoaderCallBack()
+    {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+
+
+}
+    
---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button PlayButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        PlayButton.onClick.AddListener(() =>
+        {
+            //Click
+            Loader.Load(Loader.Scene.GameScene);    
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+            //Click
+            Application.Quit();
+        });
+    }
+
+  
+}
